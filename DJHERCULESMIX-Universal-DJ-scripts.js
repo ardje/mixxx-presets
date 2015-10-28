a29ac5992b619c7c87d79402ab7fcdfc26f7d914
Ard van Breemen <ard@kwaak.net>
2015-10-28 19:08:09 +0100

    Forgot to commit, sorry

---
+++ DJHERCULESMIX-Universal-DJ-scripts.js
@@ -0,0 +1,77 @@
+function HCUniversalDJ() {};
+
+
+
+// ----------   Global variables    ---------- 
+HCUniversalDJ.deck={
+	'[Channel1]': 1,
+	'[Channel2]': 2
+};
+
+// ----------   Functions    ----------
+
+// called when the MIDI device is opened & set up
+HCUniversalDJ.init = function(id, debugging) {	
+	HCUniversalDJ.id = id;
+
+	HCUniversalDJ.allLedOff();
+	HCUniversalDJ.readFaders();
+
+	// Switch-on some LEDs for improve the usability
+	// midi.sendShortMsg(0x90, 46, 0x7F);	// Automix LED
+	// midi.sendShortMsg(0x90, 14, 0x7F);	// Cue deck A LED
+	// midi.sendShortMsg(0x90, 34, 0x7F);	// Cue deck B LED
+	print ("***** Hercules Universal DJ Control id: \""+id+"\" initialized.");
+};
+
+// Called when the MIDI device is closed
+HCUniversalDJ.shutdown = function(id) {
+	HCUniversalDJ.allLedOff();
+	print ("***** Hercules Universal DJ Control id: \""+id+"\" shutdown.");	
+};
+
+
+HCUniversalDJ.allLedOff = function () {
+	// Switch off all LEDs
+	// All deck A + 9 generic
+	for(led=1;led<95;led++) {
+		midi.sendShortMsg(0x90,led,0);
+	}
+	// All deck B
+	for(led=1;led<86;led++) {
+		midi.sendShortMsg(0x91,led,0);
+	}
+	// Deck A and B total VU meter off
+	midi.sendShortMsg(0x90,105,0);
+	midi.sendShortMsg(0x91,105,0);
+};
+
+HCUniversalDJ.readFaders = function () {
+	// reset/resend all fader values
+	midi.sendShortMsg(0xB0,127,00);
+};
+
+HCUniversalDJ.wheelTouch = function (channel, control, value, status,group) {
+    var deck=HCUniversalDJ.deck[group];
+    if (value == 0x7F && !engine.isScratching(deck)) {
+       var alpha = 1.0/8;
+       var beta = alpha/32;
+       engine.scratchEnable(deck, 128, 33+1/3, alpha, beta);
+       print ("start scratch on "+deck);
+    } else {
+       engine.scratchDisable(deck);
+       print ("stop scratch on "+deck);
+    }
+
+};
+
+HCUniversalDJ.wheelScratch = function (channel, control, value, status,group) {
+	var deck=HCUniversalDJ.deck[group];
+	if(!engine.isScratching(deck)) return;
+	var newValue;
+	if (value-64 > 0)
+		newValue = value-128;
+	else
+		newValue = value;
+	engine.scratchTick(deck,newValue);
+};
---
+++ generator.lua
@@ -0,0 +1,153 @@
+#!/usr/bin/lua
+local xml={}
+xml.head=[[
+<MixxxControllerPreset mixxxVersion="1.10.1+" schemaVersion="1">
+    <info>
+        <name>Hercules Universal DJ</name>
+        <author>Ard van Breemenr</author>
+    </info>
+    <controller id="Hercules Universel DJ">
+        <scriptfiles>
+            <file functionprefix="HCUniversalDJ" filename="DJHERCULESMIX-Universal-DJ-scripts.js"/>
+        </scriptfiles>
+]]
+xml.controls={}
+xml.outputs={}
+xml.tail=[[
+    </controller>
+</MixxxControllerPreset>
+]]
+
+local function C(desc,group,status,midino,key,options)
+	local txml=[[
+	<control>
+		<status>@_STATUS_@</status>
+		<midino>@_MIDINO_@</midino>
+		<group>@_GROUP_@</group>
+		<key>@_KEY_@</key>
+		<description>@_DESCRIPTION_@</description>
+		<options>@_OPTIONS_@
+		</options>
+	</control>
+]]
+	local control=xml.controls
+	local repl={ }
+	repl.DESCRIPTION=desc
+	repl.STATUS=status
+	repl.GROUP=group
+	repl.MIDINO=midino
+	repl.KEY=key
+	repl.KEY=key
+	repl.MIDINO=midino
+	if options ~= nil then
+		options="\n\t\t\t"..options
+	else
+		options=''
+	end
+	repl.OPTIONS=options
+	control[#control+1]=txml:gsub("@_([A-Z]+)_@",repl)
+end
+
+local function DC(desc,midino_a,midino_b,key,...)
+	C(desc.."_DA","[Channel1]","0xB0",midino_a,key,...)
+	C(desc.."_DB","[Channel2]","0xB0",midino_b,key,...)
+end
+
+local function DB(desc,midino,key,...)
+	C(desc.."_DA","[Channel1]","0x90",midino,key,...)
+	C(desc.."_DB","[Channel2]","0x91",midino,key,...)
+end
+
+local function DO(desc,midino,key)
+	local txml=[[
+	<output>
+		<status>@_STATUS_@</status>
+		<midino>@_MIDINO_@</midino>
+		<group>@_GROUP_@</group>
+		<key>@_KEY_@</key>
+		<description>@_DESCRIPTION_@</description>
+		<options>
+		</options>
+		<minimum>0.5</minimum>
+		<maximum>1</maximum>
+		<on>0x7f</on>
+		<off>0x0</off>
+	</output>
+]]
+	local repl={ }
+	local control=xml.outputs
+	repl.KEY=key
+	repl.MIDINO=midino
+	repl.DESCRIPTION=desc.."_DA" repl.GROUP="[Channel1]" repl.STATUS="0x90"
+	control[#control+1]=txml:gsub("@_([A-Z]+)_@",repl)
+	repl.DESCRIPTION=desc.."_DB" repl.GROUP="[Channel2]" repl.STATUS="0x91"
+	control[#control+1]=txml:gsub("@_([A-Z]+)_@",repl)
+end
+local function D(...)
+	DB(...)
+	DO(...)
+end
+
+local function dumpxml()
+	io.write(xml.head)
+	io.write[[
+	<controls>
+]]
+	for _,v in ipairs(xml.controls) do
+		io.write(v)
+	end
+	io.write[[
+	</controls>
+	<outputs>
+]]
+	for _,v in ipairs(xml.outputs) do
+		io.write(v)
+	end
+	io.write[[
+	</outputs>
+]]
+	io.write(xml.tail)
+end
+
+D("PFL",83,"pfl")
+DB("PLAY",65,"play")
+DO("PLAY",65,"play_indicator")
+D("SYNC",67,"beatsync")
+DB("CUE",66,"cue_default")
+DB("LOAD",81,"LoadSelectedTrack")
+C("BROWSE","[Playlist]","0xB0",54,"SelectTrackKnob","<SelectKnob/>")
+C("BROWSE","[Playlist]","0x90",92,"ToggleSelectedSidebarItem")
+DO("CUE",66,"cue_indicator")
+C("XFADER","[Master]","0xB0",65,"crossfader")
+DC("TREBLE",59,62,"filterHigh")
+DC("MEDIUM",60,63,"filterMid")
+DC("BASS",61,64,"filterLow")
+DC("VOL",57,58,"volume")
+DC("PITCH",1,2,"rate")
+DC("JOG_Scratch",50,51,"HCUniversalDJ.wheelScratch","<Script-Binding/>")
+DB("JOG_Touch",82,"HCUniversalDJ.wheelTouch","<Script-Binding/>")
+DB("CUE_MODE",65,"HCUniversalDJ.setMode","<Script-Binding/>")
+DB("FX_MODE",66,"HCUniversalDJ.setMode","<Script-Binding/>")
+DB("SAM_MODE",67,"HCUniversalDJ.setMode","<Script-Binding/>")
+DB("LOOP_MODE",68,"HCUniversalDJ.setMode","<Script-Binding/>")
+
+-- Beat loop
+function BL(n,shift,X)
+	if shift ~= 0 then
+		DB("SH_LOOP_KP"..n,56+n,"beatloop_"..X.."_toggle")
+		DO("SH_LOOP_KP"..n,56+n,"beatloop_"..X.."_enabled")
+	else
+		DB("LOOP_KP"..n,48+n,"beatloop_"..X.."_toggle")
+		DO("LOOP_KP"..n,48+n,"beatloop_"..X.."_enabled")
+	end
+end
+BL(1,0,0.125)
+BL(2,0,0.25)
+BL(3,0,0.5)
+BL(4,0,1)
+BL(5,0,2)
+BL(6,0,4)
+BL(7,0,8)
+BL(8,0,16)
+
+dumpxml()
